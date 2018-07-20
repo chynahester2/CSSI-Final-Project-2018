280dd2fc3c6130168d6a817ce0fa135096779422
Eashan Bhatt <eash130@gmail.com>
2018-07-20 17:00:26 -0400

    LInked handlers to templates
    Added address locations for different pages and created a basic template page for each page to test if they are linked correctly.

--- main.py
+++ main.py
@@ -10,8 +10,21 @@ the_jinja_environment = jinja2.Environment(
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
-        
+        welcome_template = the_jinja_environment.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
+
+class DiscountPage(webapp2.RequestHandler):
+    def get(self):
+        discount_template = the_jinja_environment.get_template('templates/discount.html')
+        self.response.write(discount_template.render())
+
+class EventsPage(webapp2.RequestHandler):
+    def get(self):
+        events_template = the_jinja_environment.get_template('templates/events.html')
+        self.response.write(events_template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', WelcomePage)
+    ('/', WelcomePage),
+    ('/discount', DiscountPage),
+    ('/events', EventsPage)
 ], debug=True)
