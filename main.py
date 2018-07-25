94944f619f3b23046689d7260a107cb2e9989f7e
Eashan Bhatt <eash130@gmail.com>
2018-07-25 16:48:53 -0400

    fixed weird things

--- main.py
+++ main.py
@@ -171,12 +171,11 @@ class MusicPage(webapp2.RequestHandler):
         music_template = the_jinja_environment.get_template('templates/music.html')
         self.response.write(music_template.render())
 
-<<<<<<< HEAD
 class MeditationPage(webapp2.RequestHandler):
     def get(self):
         meditation_template = the_jinja_environment.get_template('templates/meditation.html')
         self.response.write(meditation_template.render())
-=======
+
 class SportPage(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
@@ -275,15 +274,11 @@ class AttractionsPage(webapp2.RequestHandler):
         attractions_template= the_jinja_environment.get_template('templates/attractions.html')
         self.response.write(attractions_template.render(temp_dict))
 
->>>>>>> 2e88d37d759ef260b19e89cccc1dce95641a1758
 app = webapp2.WSGIApplication([
     ('/', WelcomePage),
     ('/discounts', DiscountPage),
     ('/events', EventsPage),
     ('/login', LoginPage),
-# <<<<<<< HEAD
-# # <<<<<<< HEAD
-# =======
     ('/books', BooksPage),
     ('/stressed', StressedPage),
     ('/museums', MuseumsPage),
@@ -292,16 +287,8 @@ app = webapp2.WSGIApplication([
     ('/dorm', DormPage),
     ('/clothes', ClothesPage),
     ('/laptop',LaptopPage),
-# <<<<<<< HEAD
-# =======
     ('/music', MusicPage),
-<<<<<<< HEAD
-# >>>>>>> 046eb23aab787640a7284b67e11a1a317fb1fe99
-=======
-    ('/music', MusicPage),
-<<<<<<< HEAD
     ('/meditation', MeditationPage),
-=======
     ('/events/sport', SportPage),
     ('/events/comedy', ComedyPage),
     ('/events/food', FoodPage),
@@ -309,5 +296,4 @@ app = webapp2.WSGIApplication([
     ('/events/music', ConcertPage),
     ('/events/theater', TheaterPage),
     ('/events/attractions', AttractionsPage)
->>>>>>> 2e88d37d759ef260b19e89cccc1dce95641a1758
 ], debug=True)
